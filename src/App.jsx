9977d5de39d6b3008c2ea051100085378648f259
nadiaaamand@gmail.com <nadiaaamand@gmail.com>
2024-10-30 10:11:55 +0100

    tilføjelse af useState og useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import {useState, useEffect} from 'react' //importere useState og useEffect hooks fra react
 import ShopInput from './ShopInput';
 import ShopList from './ShopList';
 import './App.css'
 
 function App() {
- return ShopInput();
+    const storedShopItems = JSON.parse(localStorage.getItem('shopItems')) || [];
+    const storedShops =JSON.parse(localStorage.getItem('shops')) || [];
+
+    const [shopItems, setShopItems] = useState(storedShopItems);
+    const [shops, setShops] = useState(storedShops);
+
+    //opdater localstorage når varer/shopItems ændres
+    useEffect(() => {
+        localStorage.setItem('shopItems', JSON.stringify(shopItems));
+    }, [shopItems]);
+
+    //opdater localstorage når butikker/shops ændres
+    useEffect(() =>{
+        localStorage.setItem('shops', JSON.stringify(shops))
+    }, [shops]);
+
+ return ShopInput(
+    <div>
+ <ShopInput addShopItem={addShopItem} addShop={addShop} shops={shops} />
+
+    </div>
+ );
 }
 
 export default App
--- src/ShopInput.jsx
+++ src/ShopInput.jsx
@@ -1,8 +1,35 @@
-function ShopInput(){
+//Denne fil bruges til at tilføje varer og butikker. Brugeren kan indtaste en varer, vælge afdeling og vi kan tilknytte en butik til en vare
+import {useState} from 'react';
+
+function ShopInput({addShopItem, addShop, shops}){
+    const [inputValue, setInputValue] = useState('');
+    const [priority, setPriority] = useState(''); //priority henviser til afdelingner
+    const [shopName, setShopName] = useState('');
+    const [selectedShop, setSelectedShop] = useState('');
+
+    //Håndtere data fra tilføj vare, vælg afdeling og butik inputfelt
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        if(inputValue.trim() && priority && selectedShop){
+            addShopItem(inputValue, priority, selectedShop);
+            setInputValue('');
+            setPriority('');
+            setSelectedShop('');
+        }
+    };
+
+    //håndtere data fra tilføj butik inputfelt
+    const handleShopSubmit = (e) =>{
+        e.preventDefault();
+        if(shopName.trim()){
+            addShop(shopName);
+            setShopName('');
+        }
+    };
 
     return(
         <>
-        <form>
+        <form onSubmit={handleShopSubmit}>
             <input
             type="text"
             value={shopName}
@@ -13,7 +40,7 @@ function ShopInput(){
 
         </form>
 
-        <form>
+        <form onSubmit={handleSubmit}>
             <input 
             type="text"
             value={inputValue}
